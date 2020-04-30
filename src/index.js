3772702390cdd04737cb713c8636f53fdcbdd71a
CregskiN <272473132@qq.com>
2020-04-30 09:49:53 +0800

    inheritPrototype

--- src/index.js
+++ src/index.js
@@ -1,244 +1,341 @@
-/* 1. 工厂模式 */
-// function createPerson(name, age, job) {
-//     var o = new Object();
-//     o.name = name;
-//     o.age = age;
-//     o.job = job;
-//     return o;
-// }
-
-// var person1 = createPerson('CregskiN', 20, 'Engineer');
-// // {name: "CregskiN", age: 20, job: "Engineer"}
-// var person2 = createPerson('JellyFishMix', 20, 'Doctor');
-// // {name: "JellyFishMix", age: 20, job: "Doctor"}
-// console.log(person1, person2);
-
-/* 此中方式解决实现一个接口，大量代码重复的问题。 但，如何识别对象类型，又成了一个问题 */
-
-/* 2. 构造函数模式 */
-// function Person(name, age, job) {
-//     this.name = name;
-//     this.age = age;
-//     this.job = job;
-//     this.sayName = function () {
-//         return this.name;
-//     };
-// }
-// var person1 = new Person('CregskiN', 20, 'Engineer');
-// // {name: "CregskiN", age: 20, job: "Engineer", sayName: ƒ} 
-// var person2 = new Person('JellyFishMix', 20, 'Doctor');
-// // Person {name: "JellyFishMix", age: 20, job: "Doctor", sayName: ƒ}
-// console.log(person1, person2);
-/* new Person底层步骤如下
-1. 创建一个新对象person
-2. 将构造函数Person的作用域赋给新对象person
-3. 执行构造函数Person中的代码（即为person添加属性）
-4. 返回新对象person
+/* 继承
+传统OOP语言有两种继承方式
+    1. 接口继承，只继承方法签名
+    2. 实现继承，直接继承实际方法
 */
-/* 新对象person中有一个属性指向Person */
-// console.log(person1.constructor === Person); // true
-// console.log(person2.constructor === Person); // true
-
-/* constructor最初是用来识别对象类型的，但是对象类型检测用instanceof更好 */
-// console.log(person1 instanceof Person); // true
-// console.log(person1 instanceof Object); // true
-
-/* 相比构造函数creatorPerson() 创建对象，new Person() 的构造函数模式更好，可以将实例标识为一种新类型Person*/
-
-/* 2.1 用new(当作构造函数) 与 不用new有什么区别 */
-// var person = new Person('CregskiN', 20, 'Engineer');
-// console.log(person); // Person {name: "CregskiN", age: 20, job: "Engineer", sayName: ƒ}
-
-// Person('CregskiN', 20, 'Engineer');
-// console.log(window.sayName()); // 'CregskiN' Function内this为window，name,age,job,sayName()自然绑定在了window对象
-
-// var o = new Object();
-// Person.call(o, "CregskiN", 20, 'engineer');
-// console.log(o.sayName()); // 'CregskiN' 绑定在o内。 印证了call()改变Function内的this指向
-
-/* 2.2 构造函数的问题 */
-// function Person(name, age, job) {
-//     this.name = name;
-//     this.age = age;
-//     this.job = job;
-//     this.sayName = new Function(console.log(this.name)); // 与上边等同
+/* 1. 原型链：让原型对象直接指向一个实例 */
+// function SuperType() {
+//     this.property = true;
 // }
-// var person1 = new Person('CregskiN', 20, 'Engineer');
-// var person2 = new Person('JellyFishMix', 20, 'Doctor');
-// console.log(person1.sayName === person2.sayName); // false
 
-/* 同样执行结果的两个函数，没必要定义两次。 */
-
-// function Person(name, age, job) {
-//     this.name = name;
-//     this.age = age;
-//     this.job = job;
-//     this.sayName = sayName;
+// SuperType.prototype.getSuperValue = function () {
+//     return this.property;
 // }
-// function sayName() {
-//     console.log(this.name);
+
+// function SubType() {
+//     this.subproperty = false;
 // }
-// var person1 = new Person('CregskiN', 20, 'Engineer');
-// var person2 = new Person('JellyFishMix', 20, 'Doctor');
-// person1.sayName(); // 'Cregski'
-// console.log(person1.sayName === person2.sayName); // true
+// // 继承了SuperType
+// SubType.prototype = new SuperType();
 
-/* 节省了一块sayName()的内存 */
-
-/* 新的问题：
-    1. 全局函数sayName()只有被特定的Person实例使用（只得到预期结果），
-    2.如果需要更多方法，需要定义更多全局函数，毫无封装性可言
-*/
-
-/* 3. 原型模式 */
-// function Person() { }
-// Person.prototype.name = 'CregskiN';
-// Person.prototype.age = 20;
-// Person.prototype.job = 'Engineer';
-// Person.prototype.sayName = function () {
-//     console.log(this.name);
+// SubType.prototype.getSubValue = function () {
+//     return this.subproperty;
 // }
-// var person1 = new Person('CregskiN', 20, 'Engineer');
-// var person2 = new Person('JellyFishMix', 20, 'Doctor');
-// console.log(person1 === person2); // false
+
+// var instance = new SubType();
+// console.log('实现继承，对照组', instance);
 
 /* 
-    3.1 原型对象、构造函数、实例
-    Person：构造函数
-    Person.prototype：Person的原型对象
-    Person.prototype.constructor === Person // true
+SubType {subproperty: false}
+    subproperty: false
+    __proto__: SuperType
+        getSubValue: ƒ ()
+        property: true
+        __proto__: Object 
 */
-/* Person.prototype.isPrototypeOf(person) 判断 原型对象Person.prototype是否是实例person1的 原型对象*/
-// console.log(Person.prototype.isPrototypeOf(person1)); // true
-// console.log(person1.__proto__ === Person.prototype); // true
-/* Object.getPrototypeOf(实例) 得到原型对象  */
-// console.log(Object.getPrototypeOf(person1)); // {name: "CregskiN", age: 20, job: "Engineer", sayName: ƒ, constructor: ƒ}
-// console.log(Person.prototype);// {name: "CregskiN", age: 20, job: "Engineer", sayName: ƒ, constructor: ƒ}
-// console.log(Person.prototype === Object.getPrototypeOf(person1)); // true
-/* person.hasOwnProperty() 判断是否是实例的属性 */
-// console.log(person1.hasOwnProperty('name')); // false
-// person1.name = 'aaa';
-// console.log(person1.hasOwnProperty('name')); // true
+// console.log(instance instanceof Object); // true
+// console.log(instance instanceof SuperType); // true
+// console.log(instance instanceof SubType); // true
 
-/* person.hasOwnProperty()可以判断实例属性，但如何判断一个属性，存在切存在于实例？ */
+// console.log(Object.prototype.isPrototypeOf(instance)); // true
+// console.log(SuperType.prototype.isPrototypeOf(instance)); // true
+// console.log(SubType.prototype.isPrototypeOf(instance)); // true
+
+/* 2. 方法重写 */
+// function SuperType3() {
+//     this.property = true;
+// }
+
+// SuperType3.prototype.getSuperValue = function () {
+//     return this.property;
+// }
+
+// function SubType3() {
+//     this.subproperty = false;
+// }
+// // 继承了SuperType
+// SubType3.prototype = new SuperType3();
+
+// // 添加新方法
+// SubType3.prototype.getSubValue = function () {
+//     return this.subproperty;
+// }
+// // 重写超类型中的方法
+// SubType3.prototype.getSuperValue = function () {
+//     return false;
+// }
+// var instance = new SubType3();
+// console.log('重写方法', instance);
+/* 
+SubType3 {subproperty: false}
+    subproperty: false
+    __proto__: SuperType3
+        getSubValue: ƒ ()
+        getSuperValue: ƒ ()
+        property: true
+        __proto__: Object
+*/
+
+/* 3. 原型链的问题 */
+/* 
+    1. 原型对象中的引用类型
+*/
+// function SuperType4() {
+//     this.colors = ['red', 'blue', 'green'];
+// }
+// function SubType4() { }
+// SubType4.prototype = new SuperType4();
+// var instance4_1 = new SubType4();
+// instance4_1.colors.push('black');
+// var instance4_2 = new SubType4();
+// console.log(instance4_2.colors); // ["red", "blue", "green", "black"]
+/* 
+    2. 创建字类型实例，不能向超类型的构造函数传递参数。
+    实际上，没有办法在不影响所有对象实例的情况下，给超类型的构造函数传递参数
+*/
+
 
 /* 
-    3.2 原型与in操作符
-
+    4. 借用构造函数
+        -- 解决原型对象引用类型的问题
 */
-// function Person() { }
-// Person.prototype.name = 'CregskiN';
-// Person.prototype.age = 20;
-// Person.prototype.job = 'Engineer';
-// Person.prototype.sayName = function () {
+// function SuperType5() {
+//     this.colors = ['red', 'blue', 'green'];
+// }
+// function SubType5() {
+//     SuperType5.call(this); // ! 妙不可言！
+// }
+// var instance5_1 = new SubType5();
+// instance5_1.colors.push('black');
+// console.log(instance5_1.colors); // ["red", "blue", "green", "black"]
+// var instance5_2 = new SubType5();
+// console.log(instance5_2.colors); // ["red", "blue", "green"]
+
+/* 4.1 借用构造函数的优点：传递参数 */
+// function SuperType6(name) {
+//     this.name = name;
+// }
+// function SubType6(name) {
+//     SuperType6.call(this, name);
+//     this.age = 20;
+// }
+// var instance6 = new SubType6('CregskiN');
+// console.log(instance6.name); // 'CregskiN'
+// console.log(instance6.age); // 20
+
+/* 4.2 借用构造函数的问题
+    (1) 构造函数的通病 - 函数无法复用
+    (2) 超类型中定义的方法，对子类型不可见
+*/
+
+/* 5. 组合继承 */
+// function SuperType7(name) {
+//     this.name = name;
+//     this.colors = ['red', 'blue', 'green'];
+// }
+// SuperType7.prototype.sayName = function () {
 //     console.log(this.name);
 // }
-// var person1 = new Person('CregskiN', 20, 'Engineer');
-// var person2 = new Person('JellyFishMix', 20, 'Doctor');
-// console.log(person1.hasOwnProperty('name')); // false // 'name'属性不属于实例
-// console.log('name' in person1); // true // 说明person1 可访问到属性'name'
 
-/* 综上，同时使用hasOwnProperty()和in，能判断属性属于原型对象还是实例 */
-
-/* 封装一个判断属性是否为原型属性的方法 */
-// function hasPrototypeProperty(object, property) {
-//     return !object.hasOwnProperty(property) && (property in object);
-// }
-/* !() ()
-    1   1 实例无&&能访问 => 原型属性 => true
-    1   0 实例无&&不能访问 => err => false
-    0   0 实例有&&不能访问 => err => false
-    0   1 实例有&&能访问 => 实例属性 => false
-*/
-
-// Object.defineProperty(Person.prototype, 'gender', {
-//     value: 'male',
-//     enumerable: true,
-//     writable: false,
-//     configurable: false,
-// })
-// var person3 = new Person('CregskiN', 20, 'Engineer');
-// for(let key in person3) {
-//     console.log(key); // age // name // job // sayName // (若gender.enumerable为true) gender
-// }
-/* for-in 可遍历到原型属性 */
-// console.log(hasPrototypeProperty(person3, 'gender')); // true
-
-// IE8- for-in不可枚举到[[enumerable]]: false 重写的原型方法
-// var o = {
-//     toString: () => {
-//         console.log('toString');
-//     }
-// }
-// for(let key in o){
-//     console.log(key); // toString  // IE8- undefined
+// function SubType7(name, age) {
+//     // 继承属性
+//     SuperType7.call(this, name); // 借用构造函数
+//     this.age = age;
 // }
 
+// // 继承方法(原型链模式)
+// SubType7.prototype = new SuperType7();
+// SubType7.prototype.constructor = SubType7;
+// SubType7.prototype.sayAge = function () {
+//     console.log(this.age);
+// }
+// var instance7_1 = new SubType7('CregskiN', 20);
+// instance7_1.colors.push('black');
+// console.log(instance7_1.colors); // ["red", "blue", "green", "black"]
+// instance7_1.sayName(); // 'CregskiN'
+// instance7_1.sayAge(); // 20
 
-/* Object.keys() 获取可枚举属性，不包括继承属性 */
-// console.log(Object.keys(Person.prototype)); // ["name", "age", "job", "sayName", "gender"]
-// console.log(Object.keys(person3)); // [] 实例上没有属性，也就没有可枚举属性
+// var instance7_2 = new SubType7('Cre', 30);
+// console.log(instance7_2.colors); // ["red", "blue", "green"]
+// instance7_2.sayName(); // 'Cre'
+// instance7_2.sayAge(); // 30
 
-/* Object.getOwnPropertyNames() 获取对象(原型对象，实例对象)上所有属性，不包括继承属性 */
-// console.log(Object.getOwnPropertyNames(person3)); // []
-// console.log(Object.getOwnPropertyNames(Person.prototype)); // ["constructor", "name", "age", "job", "sayName", "gender"]
+/* 组合模式 = 原型链模式 + 构造函数模式，最常用 */
 
-/* 这里的constructor属性是不可枚举的，但仍可用Object.getOwnPropertyNames()获取 */
 
-/* 3.3 字面量定义原型 */
-// function Person() { }
-// Person.prototype = {
-//     constructor: Person,
-//     name: "cregskin",
-//     age: 20,
-//     job: 'Engineer',
-//     sayName: function () {
-//         console.log(this.name);
-//     }
+/* 6. 原型式继承 */
+/* 构造一个临时类型，将传入对象作为该临时类型的原型，返回临时类型的实例 */
+// function object(o) {
+//     function F() { }
+//     F.prototype = o;
+//     return new F();
+// }
+
+// var person = {
+//     name: 'CregskiN',
+//     friends: ['a', 'b', 'c']
 // };
-// var person4 = new Person();
-// for (let key in person4) {
-//     console.log(key); // constructor // age // name // job // sayName 
-// }
+// var anotherPerson = object(person);
+// anotherPerson.name = 'Greg';
+// anotherPerson.friends.push('d');
 
-/* 字面量设置prototype相当于重写，从前[[enumerable]]:false的constructor 变成了true，这是不符合预期的 */
+// var yetAnotherPerson = object(person);
+// yetAnotherPerson.name = 'Jelly';
+// yetAnotherPerson.friends.push('e');
 
-/* 3.4 原型的动态性 */
-// function Person() {}
-// var friend = new Person();
-// Person.prototype.sayHi = function() {
-//     console.log('hi');
-// }
-// friend.sayHi(); // 'hi'
-/* Person.prototypr.sayHi 往原型添加sayHi方法，这里没毛病 */
-// function Person() {
-//     this.flag = 'Person'
-// }
-// var friend1 = new Person();
-// console.log(friend1.__proto__ === Person.prototype); // true 指向正常
-// Person.prototype = {
-//     constructor: Person,
-//     sayHi: function () {
-//         console.log('hi');
+// console.log(anotherPerson);
+/* 
+F {name: "Greg"}
+name: "Greg"
+__proto__: Object
+*/
+// console.log(yetAnotherPerson);
+/* 
+{name: "Jelly"}
+name: "Jelly"
+__proto__: Object
+*/
+// console.log(person);
+/* 
+friends: (5) ["a", "b", "c", "d", "e"]
+name: "CregskiN"
+*/
+
+/* 原型式继承，实际上对o有一个浅拷贝 */
+
+/* Object.create() ES5对原型式继承 写法的优化 */
+// var person = {
+//     name: 'CregskiN',
+//     friends: ['a', 'b', 'c']
+// };
+// var anotherPerson = Object.create(person);
+// anotherPerson.name = 'Greg';
+// anotherPerson.friends.push('d');
+
+// var yetAnotherPerson = Object.create(person);
+// yetAnotherPerson.name = 'Jelly';
+// yetAnotherPerson.friends.push('e');
+
+// console.log(anotherPerson); // {name: "Greg"}
+// console.log(yetAnotherPerson); // {name: "Jelly"}
+// console.log(person); // {name: "CregskiN", friends: Array(5)}
+
+// var person = {
+//     name: 'CregskiN',
+//     friends: ['a', 'b', 'c']
+// };
+// var anotherPerson = Object.create(person, {
+//     name: {
+//         value: 'ccc'
 //     }
+// });
+// console.log(anotherPerson);
+/* 
+{name: "ccc"}
+name: "ccc"
+__proto__:
+    friends: (3) ["a", "b", "c"]
+    name: "CregskiN"
+    __proto__: Object
+*/
+
+/* 注意：仍未解决引用类型共享的问题 */
+
+
+/* 7. 寄生式继承 */
+// function object(o) {
+//     function F() { }
+//     F.prototype = o;
+//     return new F();
 // }
-/* 以重写的方式 添加sayHi方法，有问题！ */
-// console.log(friend1.__proto__ === Person.prototype); 
-// false 重写Person.prototype后，Person的原型已经改变。但实例的隐式原型并未改变
-// 即 原型的动态性在此 作为鸡肋
-// console.log(friend1.__proto__.constructor); // ƒ Person() { this.flag = 'Person' }
-// console.log(friend1.__proto__.constructor.prototype); // {constructor: ƒ, sayHi: ƒ}
-// friend.sayHi(); // TypeError: friend.sayHi is not a function 
-/* friends1保存的__proto__是修改之前的原型对象 */
 
-// var friend2 = new Person();
-// console.log(friend2.__proto__); // {constructor: ƒ, sayHi: ƒ}
+// function createAnother(original) {
+//     var clone = object(original); // 原型继承 创建一个新对象
+//     clone.sayHi = function () { // 以某种方式增强这个对象
+//         console.log('hi');
+//     };
+//     return clone; // 返回这个对象
+// }
 
+// var person = {
+//     name: 'CregskiN',
+//     friends: ['a', 'b', 'c']
+// };
+// var anotherPerson = createAnother(person);
+// anotherPerson.sayHi(); // 'hi'
 
-/* 3.5 原生对象的原型 */
-// function 
+/* 寄生式继承的缺点：
+    1. 与构造函数相同，为对象添加的函数，无法做到函数复用
+    2. 调用两次超类型构造函数 一次创建person 一次object内创建F，属性name和friends同时存在person实例和F实例，浪费
+*/
 
+/* 8. 寄生组合式继承 */
+function object(o) {
+    function F() { }
+    F.prototype = o;
+    return new F();
+}
+function SuperType8(name) {
+    this.name = name;
+    this.colors = ['red', 'blue', 'green'];
+}
+SuperType8.prototype.sayName = function () { // 原型模式：添加方法
+    console.log(this.name);
+};
 
-/* 3.6 原型对象的问题 */
+function SubType8(name, age) {
+    SuperType8.call(this, name); // 第二次调用 SuperType()
+    this.age = age;
+}
 
+SubType8.property = new SuperType8(); // 第一次调用 SuperType()
+SubType8.prototype.constructor = SubType8;
+SubType8.prototype.sayAge = function () {
+    console.log(this.age);
+};
+
+function inheritPrototype(subType8, superType8) {
+    var prototype = object(superType8.property); // 创建对象
+    prototype.constructor = subType8; // 增强对象
+    subType8.prototype = prototype; // 指定对象
+}
+
+/* ** 这里对比ES6 class语法糖创建的实例 */
+
+class SuperType2 {
+    constructor() {
+        this.property = true;
+    }
+    getSuperValue() {
+        return this.property;
+    }
+}
+
+class SubType2 extends SuperType2 {
+    constructor() {
+        super(); // 必须
+        /* 
+        Uncaught ReferenceError: 
+            Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+            at new SubType2
+        */
+        this.subproperty = false;
+    }
+    getSubValue() {
+        return this.subproperty;
+    }
+}
+
+var instance = new SubType2();
+console.log('ES6 class', instance);
+
+/*
+SubType2 {property: true, subproperty: false}
+    property: true
+    subproperty: false
+    __proto__: SuperType2
+        constructor: class SubType2
+        getSubValue: ƒ getSubValue()
+        __proto__: Object
+*/
 
